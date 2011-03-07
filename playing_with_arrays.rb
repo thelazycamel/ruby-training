8c7e61c338899c629d554d7bd32aa1fc2f8abb13
Stuart Hanscombe <stuart.hanscombe@vcint.com>
2011-03-07 22:42:57 +0100

    playing with arrays and regular expressions

---
+++ playing_with_arrays.rb
@@ -0,0 +1,25 @@
+array1 = [1, 2, 3, 4]
+array2 = %w{peter piper picked peppers}
+array3 = []
+
+
+#attempt number 1 (across multiple lines)
+array1.each do |position|
+  array3 << array1[position -1].to_s
+  array3 << array2[position -1]
+end
+
+#attempt number 2 (on one line) - tough so shuffle them up and hope for the best :)
+array3 = (array1 + array2).map{|x| x.to_s}.shuffle
+
+#attempt number 3 (on one line) - the most complicated one-line ive ever done.
+array3 = array1.map{|x| x.to_s + "," + array2[x-1].to_s + ","}.join.split(",")
+
+puts array3.inspect
+
+
+if array3 == ['1', 'peter', '2', 'piper', '3', 'picked', '4', 'peppers']
+  puts "Success"
+else
+  puts "Failed"
+end
