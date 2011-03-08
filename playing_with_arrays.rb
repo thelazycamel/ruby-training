cdc003931ac9500a004b271e562768a4f08cd1c0
Stuart Hanscombe <stuart.hanscombe@vcint.com>
2011-03-08 21:22:35 +0100

    much nicer - putting the attempts into lambdas

--- playing_with_arrays.rb
+++ playing_with_arrays.rb
@@ -1,25 +1,34 @@
 array1 = [1, 2, 3, 4]
 array2 = %w{peter piper picked peppers}
-array3 = []
 
-
-#attempt number 1 (across multiple lines)
-array1.each do |position|
-  array3 << array1[position -1].to_s
-  array3 << array2[position -1]
+class ArrayTesting
+  
+  def test(array3)
+    if array3 == ['1', 'peter', '2', 'piper', '3', 'picked', '4', 'peppers']
+      "A Total Success"
+    else
+      "A Complete Failure"
+    end
+  end
+    
 end
 
-#attempt number 2 (on one line) - tough so shuffle them up and hope for the best :)
-array3 = (array1 + array2).map{|x| x.to_s}.shuffle
-
-#attempt number 3 (on one line) - the most complicated one-line ive ever done - i guess thats what you meant by zip it?
-array3 = array1.map{|x| x.to_s + "," + array2[x-1].to_s + ","}.join.split(",")
-
-puts array3.inspect
-
-
-if array3 == ['1', 'peter', '2', 'piper', '3', 'picked', '4', 'peppers']
-  puts "Success"
-else
-  puts "Failed"
+attempt1 = lambda do |array1,array2|
+ array3 = []
+  4.times do |i|
+  array3 << array1[i].to_s
+  array3 << array2[i]
+  end
+ return array3
 end
+
+attempt2 = lambda {|array1, array2 | (array1 + array2).map{|x| x.to_s}.shuffle}
+
+attempt3 = lambda {|array1,array2| array1.map {|x| x.to_s}.zip(array2).flatten}
+ 
+puts "The result for attempt number 1 is #{ArrayTesting.new.test(attempt1.call(array1,array2))}"
+puts "The result for attempt number 2 is #{ArrayTesting.new.test(attempt2.call(array1,array2))}"
+puts "The result for attempt number 3 is #{ArrayTesting.new.test(attempt3.call(array1,array2))}"
+
+p attempt2.call(array1,array2)
+
